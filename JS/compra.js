a8a5dc4e768de38e27dc3f932c702481c1a8a836
Luis219 <79708823+Luis219@users.noreply.github.com>
2021-09-09 00:18:15 -0500

    Create compra.js

---
+++ JS/compra.js
@@ -0,0 +1,26 @@
+function mostrarCheckboxes() {
+    
+    let checkboxes = document.getElementById("checkboxes");
+ 
+    if(checkboxes.classList.contains("hide")) {
+        checkboxes.classList.remove("hide");
+      
+      
+
+        
+    } else {
+        checkboxes.classList.add("hide");
+       
+       
+    }
+}
+
+function mostrarPrecios(){
+    let precios = document.getElementById("precios");
+        if(precios.classList.contains("hidePrecios")) {
+            precios.classList.remove("hidePrecios");
+        } else {
+            precios.classList.add("hidePrecios");
+        }
+
+}
